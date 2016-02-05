74dd63aed35cf2a48c3a672f7d6a51af36e1ba96
Genevieve Latimer <genevievelatimer@gmail.com>
2016-02-05 15:23:41 -0800

    Add JS for new flashcards.

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,34 @@
 $(document).ready(function() {
-  $(".clickable").click(function() {
-    $("#hidden h2").toggle();
+  $(".js").click(function() {
+    $("#showing").toggle();
+    $("#hidden").toggle();
+  });
+});
+
+$(document).ready(function() {
+  $(".modulo").click(function() {
+    $("#showing-modulo").toggle();
+    $("#hidden-modulo").toggle();
+  });
+});
+
+$(document).ready(function() {
+  $(".NaN").click(function() {
+    $("#showing-NaN").toggle();
+    $("#hidden-NaN").toggle();
+  });
+});
+
+$(document).ready(function() {
+  $(".var").click(function() {
+    $("#showing-var").toggle();
+    $("#hidden-var").toggle();
+  });
+});
+
+$(document).ready(function() {
+  $(".dom").click(function() {
+    $("#showing-dom").toggle();
+    $("#hidden-dom").toggle();
   });
 });
